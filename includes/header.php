9cd9420bd637cc3d66ae8b06f084f1d7e6e55865
Arthurgdct <arthurgrandclement@gmail.com>
2025-11-12 18:34:23 +0100

    commit initial & squelette du site.

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Paumalec</title>
+</head>
+<body>
+    <header id="bg-blue">
+        <div>
+            <a href="#"><img src="./assets/img/LogoPaumalec" alt="Logo de l'entreprise."></a>
+        </div>
+        <div>
+            <nav>
+<!-- En attente d'information pour la navbar -->
+            </nav>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+include 'controllers/indexctrl.php'
+    ?>
+<main>
+    <section id="accueil">
+        <div>
+            <h1>Paumalec Spécialisé dans le secteur tertiaire</h1>
+            <div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, cumque? Aliquam provident neque
+                    laboriosam. Nulla, maiores debitis consequuntur iure corporis nisi aspernatur atque ratione cumque
+                    pariatur fugiat nemo culpa? Maiores!</p>
+                <button>Contact</button>
+            </div>
+            <div>
+                <h2>Lorem ipsum dolor sit</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quam sunt ullam error nemo
+                    veritatis! Consequuntur quae ipsum soluta doloremque, dolorum officia nobis fugiat velit amet vel
+                    laboriosam sit voluptates.</p>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div>
+            <h3>Qui sommes nous ?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem provident corrupti a eos, eligendi
+                labore. Eaque expedita, magnam dignissimos aperiam, quaerat magni minima vero deleniti iure repellendus
+                placeat. Veniam, quisquam!</p>
+        </div>
+        <div>
+            <img src="assets/img/electricien" alt="Photo d'un électricien de l'entreprise en pleine intervention.">
+            <div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum rerum rem eum maiores temporibus, dicta
+                    magnam, commodi dignissimos vel consectetur debitis dolores mollitia molestiae fugiat possimus
+                    asperiores doloremque ut fuga? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+                    unde illum aperiam facere vitae perspiciatis incidunt quos exercitationem dolore, dolores, hic quas!
+                    Aliquam possimus eaque, tenetur deserunt deleniti illo quam! Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Inventore unde quidem sapiente ipsam consectetur minima pariatur, doloremque optio
+                    eos sequi sit aliquam maxime, ut sunt suscipit dolores architecto quo! Adipisci. Lorem ipsum dolor
+                    sit amet consectetur adipisicing elit. Eveniet cumque nobis, impedit eius sit dignissimos rerum eos
+                    est ad enim doloremque doloribus ratione iure, ea voluptas maxime vero eligendi adipisci.</p>
+                <button></button>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div>
+            <h4>Ils nous font confiance</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt consectetur unde vel dignissimos, sunt
+                iste odio culpa neque quos. Minima officia odit placeat animi natus, aperiam harum enim alias! Natus?
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique iure exercitationem ab tempore.
+                Consequuntur harum voluptas, facere quasi unde eaque ipsa saepe, nemo asperiores dolores obcaecati error
+                neque, ratione ipsum!</p>
+            <div>
+                <img src="assets/img/EiffageGC" alt="Logo Eiffage génie civil">
+                <img src="assets/img/logo-pum" alt="Logo PUM">
+                <img src="assets/img/Renault" alt="Logo Renault">
+                <img src="assets/img/Sourcesdallauch" alt="Logo Sources d'allauche">
+                <img src="assets/img/Edf" alt="Logo Edf">
+                <img src="assets/img/EiffageR" alt="Logo Eiffage Route">
+                <img src="assets/img/Logo_ecole_lacordaire" alt="Logo Ecole lacordaire">
+                <img src="assets/img/VisualOpticien" alt="Logo Visual Opticien">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div>
+            <h5>Nos Services</h5>
+        </div>
+        <div>
+            <div>
+                <p>Electricité général</p>
+            </div>
+            <div>
+                <div>
+                    <p>climatisation</p>
+                </div>
+                <div>
+                    <p>système de sécurité</p>
+                </div>
+                <div>
+                    <p>Borne de recharge</p>
+                </div>
+                <div>
+                    <p>programmation</p>
+                </div>
+                <div>
+                    <p>Volet roulant</p>
+                </div>
+                <div>
+                    <p>Electricité solaire</p>
+                </div>
+                <div>
+                    <p>Portail</p>
+                </div>
+                <div>
+                    <p>Base vie</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <h6>Nos réalisations</h6>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed esse alias perspiciatis molestiae blanditiis
+            saepe cupiditate voluptas molestias sapiente totam, quis iusto culpa laborum! Unde aut possimus nesciunt eum
+            vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque totam, optio nisi vitae provident
+            illum, aut excepturi dolores est, quaerat cumque. Quisquam quasi eos animi id non, similique ducimus porro.
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur tenetur atque harum excepturi rerum
+            est earum numquam, modi aliquid debitis tempora autem, totam at cupiditate incidunt eligendi ipsum magni?
+            Magni.</p>
+        <div>
+            <div>
+                <img src="assets/img/1avant" alt="">
+                <p>Avant</p>
+            </div>
+            <div>
+                <img src="assets/img/2apres" alt="">
+                <p>Après</p>
+            </div>
+            <div>
+                <img src="assets/img/3avant" alt="">
+                <p>Avant</p>
+            </div>
+            <div>
+                <img src="assets/img/4apres" alt="">
+                <p>Après</p>
+            </div>
+        </div>
+        <button>Nos réalisations</button>
+    </section>
+    <section>
+        <h6>Devis</h6>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. In hic excepturi saepe quis ea deleniti tempore.
+            Quasi, totam molestiae corporis magnam commodi tempore, provident corrupti consequatur officia laborum,
+            aperiam beataem.</p>
+        <form action="index.php" method="post">
+            <div>
+                <input type="text" name="nom" id="nom" placeholder="Nom">
+                <input type="text" name="societe" id="societe" placeholder="Société">
+            </div>
+            <input type="email" name="email" id="email" placeholder="E-mail">
+            <input type="text" name="message" id="message">
+            <input type="submit" name="validform" value="Envoyer">
+        </form>
+    </section>
+</main>
