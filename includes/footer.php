99b96c09f348a99bc7755e582a9bac011cba62fe
Arthurgdct <arthurgrandclement@gmail.com>
2025-11-18 16:07:41 +0100

    Ajout du squelette du footer & des logos réseaux

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+<footer>
+    <div>
+        <img src="./assets/img/LogoPaumalec.png" alt="Logo de l'entreprise Paumalec">
+        <div>
+            <a href="#">Qui sommes nous ?</a>
+            <a href="">Ils nous font confiance</a>
+            <a href="">Nos services</a>
+            <a href="">Nos réalisations</a>
+            <a href="">Devis</a>
+            <a href="">Contact</a>
+            <a href="">Avis</a>
+        </div>
+        <div>
+            <a href="">FAQ</a>
+            <a href="">Privacy Policy</a>
+            <a href="">Cookies</a>
+        </div>
+    </div>
+    <div>
+        <img src="./assets/img/Smartphone" alt="">
+    </div>
+    <div>
+        <p>Copyright | Paumalec</p>
+        <div>
+            <img src="./assets/img/Instagram" alt="Logo Instagram">
+            <img src="./assets/img/Facebook" alt="Logo Facebook">
+            <img src="./assets/img/X" alt="Logo X">
+        </div>
+    </div>
+</footer>
